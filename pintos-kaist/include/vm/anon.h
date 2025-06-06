45d3caa2ee59d6f12398b6478e627d99e423e8b3
yoominlee00 <yumini@gmail.com>
2025-06-06 22:26:08 +0900

    Add: swapin/out

--- pintos-kaist/include/vm/anon.h
+++ pintos-kaist/include/vm/anon.h
@@ -1,11 +1,21 @@
 #ifndef VM_ANON_H
 #define VM_ANON_H
 #include "vm/vm.h"
+#include "devices/disk.h"
+#include "include/threads/vaddr.h"
+#include "include/lib/kernel/bitmap.h"
+#include "threads/synch.h"
+
 struct page;
 enum vm_type;
 
 struct anon_page {
-    
+    int swap_slot_idx;
+};
+
+struct swap_table {
+    struct bitmap *swap_bitmap;
+    struct lock swap_lock;
 };
 
 void vm_anon_init (void);
